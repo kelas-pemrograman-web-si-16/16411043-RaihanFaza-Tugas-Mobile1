d0e0920c55a0483e22b8d501a709ab72444b6641
raihanfaza <raihanmkt25@gmail.com>
2019-03-17 13:06:10 +0700

    tugasmobile1

---
+++ app/src/main/java/com/example/tugas1mobile/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tugas1mobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("tugas1mobile");
+    }
+}
